b0292c7116474eee35f2d2f892a2155acf01b2cb
Giurgila Alexandru <giurgila_alexandru@yahoo.com>
2020-04-01 17:32:52 +0300

    Added lab 7

---
+++ Lab7/WCF/FileProperty.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyPhotos;
+
+namespace WCF
+{
+    public class FileProperty : IFileProperty
+    {
+        public void Add(File file)
+        {
+            file.Add(file);
+        }
+
+        public void Add(Property property)
+        {
+            property.Add(property);
+        }
+
+        void InterfaceFile.Delete(Guid id)
+        {
+            File file = new File();
+            file.Delete(id);
+        }
+
+        void InterfaceProperty.Delete(Guid id)
+        {
+            Property property = new Property();
+            property.Delete(id);
+        }
+
+
+        public List<File> GetAllFiles()
+        {
+            File file = new File();
+            return file.GetAllFiles();
+        }
+
+        public List<string> GetAllProperties()
+        {
+            Property property = new Property();
+            return property.GetAllProperties();
+        }
+
+        public List<File> GetFilesByProperty(string key, string value)
+        {
+            File file = new File();
+            return file.GetFilesByProperty(key, value);
+        }
+
+        public List<Property> GetPropertiesByFileId(Guid fileId)
+        { 
+            Property property = new Property();
+            return property.GetPropertiesByFileId(fileId);
+        }
+
+        public void Update(File file)
+        {
+            file.Update(file);
+        }
+
+        public void Update(Property property)
+        {
+            property.Update(property);
+        }
+    }
+}
---
+++ Lab7/WCF/InterfaceWCF.cs
@@ -0,0 +1,45 @@
+﻿using MyPhotos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF
+{
+    [ServiceContract]
+    interface InterfaceFile
+    {
+        [OperationContract]
+        void Add(File file);
+        [OperationContract]
+        List<File> GetAllFiles();
+        [OperationContract]
+        List<File> GetFilesByProperty(string key, string value);
+        [OperationContract]
+        void Update(File file);
+        [OperationContract]
+        void Delete(Guid id);
+    }
+
+    [ServiceContract]
+    interface InterfaceProperty
+    {
+        [OperationContract]
+        void Add(Property property);
+        [OperationContract]
+        List<String> GetAllProperties();
+        [OperationContract]
+        List<Property> GetPropertiesByFileId(Guid fileId);
+        [OperationContract]
+        void Update(Property property);
+        [OperationContract]
+        void Delete(Guid id);
+    }
+
+    [ServiceContract]
+    interface IFileProperty : InterfaceFile, InterfaceProperty
+    {
+    }
+}
