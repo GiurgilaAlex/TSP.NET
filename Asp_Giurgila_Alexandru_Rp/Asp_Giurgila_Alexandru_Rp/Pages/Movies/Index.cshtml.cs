88cb0060b64ecf2b030b51405a33c6b36936a7d6
Giurgila Alexandru <giurgila_alexandru@yahoo.com>
2020-05-06 16:25:40 +0300

    Added Asp_Giurgila_Alexandru_Rp

---
+++ Asp_Giurgila_Alexandru_Rp/Asp_Giurgila_Alexandru_Rp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Asp_Giurgila_Alexandru_Rp.Data;
+using Asp_Giurgila_Alexandru_Rp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Asp_Giurgila_Alexandru_Rp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Asp_Giurgila_Alexandru_Rp.Data.Asp_Giurgila_Alexandru_RpContext _context;
+
+        public IndexModel(Asp_Giurgila_Alexandru_Rp.Data.Asp_Giurgila_Alexandru_RpContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+        public async Task OnGetAsync()
+        {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
